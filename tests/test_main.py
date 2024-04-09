97bb931fde6b92bbeb7744bff9fd40ff0a5ce796
Jake Cyr <cyrjake@gmail.com>
2024-04-09 08:54:44 -0400

    Add another test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,26 @@
 from __future__ import annotations
-import pytest
 from toml_to_requirements.main import main
 from toml_to_requirements.cli import CLIArguments
 from pathlib import Path
+
+EXAMPLE_TOML_PATH_WITH_VERSIONS = Path("tests/example_toml_with_versions.toml")
+EXAMPLE_REQUIREMENTS_PATH_WITH_VERSIONS = Path(
+    "tests/example_requirements_with_versions.txt"
+)
 
 EXAMPLE_TOML_PATH = Path("tests/example_toml.toml")
 EXAMPLE_REQUIREMENTS_PATH = Path("tests/example_requirements.txt")
 
 
-@pytest.fixture()
-def arguments() -> CLIArguments:
-    return CLIArguments(
-        toml_file_path=EXAMPLE_TOML_PATH,
-        requirements_file_path=EXAMPLE_REQUIREMENTS_PATH,
-        optional_lists=["dev"],
-        poetry=False,
-    )
-
-
-def test_main(arguments: CLIArguments) -> None:
+def test_main_with_simple_toml() -> None:
     try:
+        arguments = CLIArguments(
+            toml_file_path=EXAMPLE_TOML_PATH,
+            requirements_file_path=EXAMPLE_REQUIREMENTS_PATH,
+            optional_lists=["dev"],
+            poetry=False,
+        )
+
         main(arguments)
 
         assert EXAMPLE_REQUIREMENTS_PATH.is_file()
@@ -34,3 +35,28 @@ def test_main(arguments: CLIArguments) -> None:
         assert len(packages_in_requirements) == 2
     finally:
         EXAMPLE_REQUIREMENTS_PATH.unlink(missing_ok=True)
+
+
+def test_main_with_versions_in_toml() -> None:
+    try:
+        arguments = CLIArguments(
+            toml_file_path=EXAMPLE_TOML_PATH_WITH_VERSIONS,
+            requirements_file_path=EXAMPLE_REQUIREMENTS_PATH_WITH_VERSIONS,
+            optional_lists=["dev"],
+            poetry=False,
+        )
+
+        main(arguments)
+
+        assert EXAMPLE_REQUIREMENTS_PATH_WITH_VERSIONS.is_file()
+
+        with EXAMPLE_REQUIREMENTS_PATH_WITH_VERSIONS.open("r") as f:
+            requirements_content: str = f.read()
+
+        packages_in_requirements = set(requirements_content.split("\n"))
+
+        assert "black>=3.1,<4.0" in packages_in_requirements
+        assert "toml==2.0" in packages_in_requirements
+        assert len(packages_in_requirements) == 2
+    finally:
+        EXAMPLE_REQUIREMENTS_PATH_WITH_VERSIONS.unlink(missing_ok=True)
